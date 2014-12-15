5f572ce6002dad258846995bb1d50d088571ee3e
Damian Romanów <damian.romanow@gmail.com>
2014-12-15 23:58:56 +0100

    categories migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210115848) do
+ActiveRecord::Schema.define(version: 20141215212356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "icon"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.datetime "created_at"
@@ -33,8 +40,10 @@ ActiveRecord::Schema.define(version: 20141210115848) do
     t.string   "title"
     t.text     "body"
     t.integer  "owner_id"
+    t.integer  "category_id"
   end
 
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
   add_index "posts", ["owner_id"], name: "index_posts_on_owner_id", using: :btree
 
   create_table "users", force: true do |t|
