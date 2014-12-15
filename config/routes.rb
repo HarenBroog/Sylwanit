7bf573f336209a03433494f4c095773cd679d52e
Damian Romanów <damian.romanow@gmail.com>
2014-12-15 23:59:23 +0100

    categories resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  resources :categories
+
   devise_for :users
 
   resources :users do
     resources :posts
     resources :comments
+  end
+
+  resources :categories do
+    resources :posts
   end
 
   resources :posts do
