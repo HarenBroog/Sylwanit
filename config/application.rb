8ed843634d92d67c6263a3a8dbb514d1419b6f32
Damian Romanów <damian.romanow@gmail.com>
2015-01-05 14:29:11 +0100

    precompiled icons

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Sylwanit
      config.sass.preferred_syntax = :sass
      config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
      config.assets.paths << Rails.root.join('public', 'assets', 'font-awesome')
+     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
