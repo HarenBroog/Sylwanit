9f48a7feea6ddaab96fbc0322735c9329710a772
Damian Romanów <damian.romanow@gmail.com>
2015-01-07 19:41:40 +0100

    provide default avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
   def background_url
-    url = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}/assets/backgrounds/"
+    url = "#{s3_asset_url}/backgrounds/"
     return image_url(url + "background-green.jpg") unless current_user
     image_url(url + current_user.background_url)
+  end
+
+  def s3_asset_url
+    "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}/assets"
   end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,10 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+  include ApplicationHelper
+
+  def default_url
+    "#{s3_asset_url}/avatar/thumb_avatar_default.png"
+  end
 
   def store_dir
     "#{Rails.env}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
