bb47c25775955123edcc22b57d7a77a4e396a013
Damian Romanów <damian.romanow@gmail.com>
2015-01-07 19:06:19 +0100

    use backgrounds from s3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def background_url
-    return image_url("background-green.jpg") unless current_user
-    image_url(current_user.background_url)
+    url = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}/assets/backgrounds/"
+    return image_url(url + "background-green.jpg") unless current_user
+    image_url(url + current_user.background_url)
   end
 end
